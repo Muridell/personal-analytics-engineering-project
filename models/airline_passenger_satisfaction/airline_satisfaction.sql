c5ae1f8ed9b6e81d3a43aac07ab9a28f5aff4b39
Oduntan Murtadha <muridel199@gmail.com>
2023-03-19 00:45:28 +0000

    airline passenger age category

--- models/airline_passenger_satisfaction/airline_satisfaction.sql
+++ models/airline_passenger_satisfaction/airline_satisfaction.sql
@@ -11,6 +11,9 @@ WITH passenger_satisfaction as (
 SELECT  ID
         ,GENDER
         ,AGE
+        ,CASE WHEN AGE BETWEEN 1 AND 12 THEN 'Children'
+                WHEN AGE > 12 THEN 'Adult'
+        END AGE_CATEGORY
         ,CUSTOMER_TYPE
         ,TYPE_OF_TRAVEL
         ,CLASS
@@ -42,6 +45,9 @@ FROM
 GROUP BY ID
         ,GENDER
         ,AGE
+        ,CASE WHEN AGE BETWEEN 1 AND 12 THEN 'Children'
+                WHEN AGE > 12 THEN 'Adult'
+        END 
         ,CUSTOMER_TYPE
         ,TYPE_OF_TRAVEL
         ,CLASS
