64042520dca063d21bddcced351f8d14adab658a
Oduntan Murtadha <muridel199@gmail.com>
2023-03-23 10:59:12 +0000

    update marketing

--- models/marketing/marketing.sql
+++ models/marketing/marketing.sql
@@ -5,3 +5,33 @@
         tags = ['marketing']
     )
 }}
+
+SELECT ID
+        ,YEAR_BIRTH
+        ,EDUCATION
+        ,MARITAL_STATUS
+        ,INCOME
+        ,KIDHOME
+        ,TEENHOME
+        ,DT_CUSTOMER
+        ,RECENCY
+        ,MNT_WINES
+        ,MNT_FRUITS
+        ,MNT_MEAT_PRODUCTS
+        ,MNT_FISH_PRODUCTS
+        ,MNT_SWEET_PRODUCTS
+        ,MNT_GOLD_PRODS
+        ,NUM_DEALS_PURCHASES
+        ,NUM_WEB_PURCHASES
+        ,NUM_CATALOG_PURCHASES
+        ,NUM_STORE_PURCHASES
+        ,NUM_WEB_VISITS_MONTH
+        ,ACCEPTED_CMP_3
+        ,ACCEPTED_CMP_4
+        ,ACCEPTED_CMP_5
+        ,ACCEPTED_CMP_1
+        ,ACCEPTED_CMP_2
+        ,RESPONSE
+        ,COMPLAIN
+        ,COUNTRY
+FROM {{source('marketing', 'MARKETING_DATA')}}
